8ed42d2edfff5ac6eff62ee12dbe59493cef7634
nampdn <nampdn@protonmail.com>
2019-10-21 20:35:42 +0700

    chore(clean): old src

--- src/seep/route.ts
+++
@@ -1,9 +0,0 @@
-import { WhenRequest } from '../types'
-
-export const hasQuery = (query?: string) => ({ request }: WhenRequest) => {
-  return !!request.query && query && query in request.query
-}
-
-export const hasPath = (path?: string) => ({ request }: WhenRequest) => {
-  return request.originalUrl === path
-}
